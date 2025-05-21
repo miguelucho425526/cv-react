4350ef9e07849abf306c46fddc275f2e698c35f2
Estiben <jhon.mosquerapalacios@inemjose.edu.co>
2025-05-21 14:21:07 -0500

    segundo commit descripcion del perfil

--- src/Components/Perfil.jsx
+++ src/Components/Perfil.jsx
@@ -4,7 +4,7 @@ export default function Perfil() {
       <h2>Perfil</h2>
       <p>
         Tecnólogo en Análisis y Desarrollo de Software con experiencia en desarrollo web y móvil. 
-        Apasionado por la tecnología, con habilidades en lenguajes como Python y Java, 
+        Apasionado por la tecnología, con habilidades en lenguajes como Python, javascript, php, Java y bases de datos 
         y un alto compromiso con la puntualidad en la entrega de proyectos.
       </p>
     </section>
