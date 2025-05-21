b72b8c24c961f323beb945ebb36b81faf4a56ce2
Yeison <ycaroorrego@gmail.com>
2025-05-21 13:27:46 -0500

    feat: componente CabeceraCV con datos personales actualizados

---
+++ src/Components/CabeceraCV.jsx
@@ -0,0 +1,10 @@
+export default function CabeceraCV() {
+  return (
+    <header>
+      <h1>Yeison Caro</h1>
+      <h2>Desarrollador Back-End </h2>
+      <h3>+57 3014420725</h3>
+      <h4>ycaroorrego@gmail.com</h4>
+    </header>
+  );
+}
