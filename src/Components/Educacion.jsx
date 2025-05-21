4c230fe870b4788719bb96a74b8b4b396979f72b
Estiben <jhon.mosquerapalacios@inemjose.edu.co>
2025-05-21 14:30:38 -0500

    cuarto commit educacion

--- src/Components/Educacion.jsx
+++ src/Components/Educacion.jsx
@@ -10,7 +10,7 @@ export default function Educacion() {
           <strong>Tecnólogo en Análisis y Desarrollo de Software</strong> – SENA (2016–2018)
         </li>
         <li>
-          <strong>Curso: Fundamentos de JavaScript</strong> – Platzi (2023)
+          <strong>Curso: Ciberseguridad, uso de linux, scripts, hacking wifi</strong> – Hackforu (2023)
         </li>
         <li>
           <strong>Curso: Desarrollo Web con React</strong> – Udemy (2024)
@@ -19,7 +19,7 @@ export default function Educacion() {
           <strong>Taller: Introducción a la Ciberseguridad</strong> – Cisco Networking Academy (2022)
         </li>
         <li>
-          <strong>Seminario: Diseño de Interfaces UX/UI</strong> – Google Activate (2023)
+          <strong>Curso: bounty hunting, hackingweb y vulnerabilidades de sitios</strong> – hackforu (2023)
         </li>
       </ul>
     </section>
--- src/Components/Perfil.jsx
+++ src/Components/Perfil.jsx
@@ -6,6 +6,7 @@ export default function Perfil() {
         Tecnólogo en Análisis y Desarrollo de Software con experiencia en desarrollo web y móvil. 
         Apasionado por la tecnología, con habilidades en lenguajes como Python, javascript, php, Java y bases de datos 
         y un alto compromiso con la puntualidad en la entrega de proyectos.
+        Conocimientos varios en Ciberseguridad y hackingwifi
       </p>
     </section>
   );
