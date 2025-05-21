ebdb93110c96350b363c1f54af65358253bb7cf5
Estiben <jhon.mosquerapalacios@inemjose.edu.co>
2025-05-21 14:26:41 -0500

    tercer commit experiencia

--- src/Components/Experiencia.jsx
+++ src/Components/Experiencia.jsx
@@ -4,20 +4,20 @@ export default function Experiencia() {
       <h3>Experiencia Profesional</h3>
       <ul>
         <li>
-          <strong>Desarrollador Frontend</strong> – TechNova Solutions (2022–2024)
-          <p>Diseño e implementación de interfaces web responsivas usando React y Tailwind. Integración con APIs REST y optimización del rendimiento.</p>
+          <strong>Desarrollador Fullstack</strong> – Inemsite (2022–2024)
+          <p>Diseño e implementación de interfaces web responsivas usando React y Tailwindcss. Integración con APIs REST y optimización del rendimiento.</p>
         </li>
         <li>
           <strong>Instructor TIC</strong> – SENA (2020–2022)
           <p>Formación en desarrollo web y programación básica. Asesoría en proyectos académicos y soporte técnico en plataformas educativas.</p>
         </li>
         <li>
-          <strong>Desarrollador Full Stack</strong> – Proyecto Freelance (2024)
+          <strong>Desarrollador Full Stack</strong> – Proyecto Freelance (2025)
           <p>Creación de una plataforma de gestión de hojas de vida con Node.js y React. Implementación de login, generación de PDFs y conexión a base de datos MongoDB.</p>
         </li>
         <li>
-          <strong>Diseñador UI/UX</strong> – NeoPixel Studio (2021)
-          <p>Diseño de interfaces y prototipos interactivos con Figma para apps móviles. Colaboración con desarrolladores en la implementación visual.</p>
+          <strong>Encargado del backend</strong> – techmate solutions (2022)
+          <p>creacion de API rest para techmate solutions, diseño de bases de datos y la logica del proyecto.</p>
         </li>
       </ul>
     </section>
